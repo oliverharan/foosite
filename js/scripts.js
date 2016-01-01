b5bd932137e1fb00a96ad23568a0fef1ca17e1f8
oliverharan <harano@live.com>
2015-12-31 21:05:41 -0500

    Callout box Fix (add h4 tag to hover elements to prevent skewing). javascript Callout fix (correct path) add Callout box images add site images

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-(function($){
- 
- 
+$(document).ready(function(){ 
  
  $('.dropdown .dropdown-menu').on({
 	"click":function(e){
@@ -17,9 +15,8 @@ $('.col-md-4:nth-child(1) > div').hover(function() {
 });
   $('.col-md-4:nth-child(2) > div').hover(function() {
     //$('body > header > div > div > div > div:nth-child(1) > div > div.overlay > div:nth-child(1)').slideToggle(270);
-  $('.col-md-4:nth-child(2) > div > div.overlay > div:nth-child(2)').toggle( "ease");
+  $('.col-md-4:nth-child(2) > div > div.overlay > div:nth-child(1)').toggle( "ease");
 });
  
+});    
     
-    
-})(jQuery);
